dea8abb9def81f44a9702a46f5e422c808c0debb
Lyudmila Moskvenkova <lmoskvenkova@appulate.com>
2020-12-23 14:39:27 +0300

    new project

---
+++ File2ByteConverter/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace File2ByteConverter {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			byte[] bytes = File.ReadAllBytes(textBox1.Text);
+			string res1 = string.Join("", bytes.Select(b => b.ToString()));
+			string res2 = Convert.ToString(bytes);
+			string res3 = Encoding.ASCII.GetString(bytes);
+			richTextBox1.Text = res1;
+		}
+	}
+}
